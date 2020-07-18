57fe8d32026bfe01f6ab1f19325c51005b5fd095
vishnu <VISHNUPRAKASHREDDY@GMAIL.COM>
2020-07-18 15:38:09 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tQjpcQ2CcHe62DAoX3npWm5SHMg5wkOjbDsLmZklKvE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
